dd31376f5b739c43673b11345012e74a34380b82
Zhejian-Zheng <zj.zheng1@gmail.com>
2026-06-17 00:00:04 +1000

    Initialize Spring Boot project

---
+++ src/test/java/com/clearledger/clearledger/ClearLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clearledger.clearledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearLedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
